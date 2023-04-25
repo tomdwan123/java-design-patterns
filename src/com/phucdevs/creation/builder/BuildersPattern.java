f7814e65a0ef276d9d74f47cb1cbc7e6c8a32e72
phuc.letan <phuc.letan@messagemedia.com>
2023-04-25 14:56:16 +0700

    Builders Pattern example

--- src/com/phucdevs/creation/builder/BuildersPattern.java
+++ src/com/phucdevs/creation/builder/BuildersPattern.java
@@ -3,81 +3,92 @@ package com.phucdevs.creation.builder;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class BuildersPattern {
+class HtmlElement
+{
+    public String name, text;
+    public ArrayList<HtmlElement> elements = new ArrayList<HtmlElement>();
+    private final int indentSize = 2;
+    private final String newLine = System.lineSeparator();
 
-    static class HtmlElement {
-
-        public String name, text;
-        public ArrayList<HtmlElement> elements = new ArrayList<>();
-        private final int indentSize = 2;
-        private final String newLine = System.lineSeparator();
-
-        public HtmlElement() {}
-
-        public HtmlElement(String name, String text) {
-            this.name = name;
-            this.text = text;
-        }
-
-        public HtmlElement(String name, String text, ArrayList<HtmlElement> elements) {
-            this.name = name;
-            this.text = text;
-            this.elements = elements;
-        }
-
-        private String toStringImpl(int indent) {
-
-            StringBuilder sb = new StringBuilder();
-            String i = String.join("", Collections.nCopies(indent * indentSize, " "));
-            sb.append(String.format("%s<%s>%s", i, name, newLine));
-            if (text != null && !text.isEmpty())
-            {
-                sb.append(String.join("", Collections.nCopies(indentSize*(indent+1), " ")))
-                        .append(text)
-                        .append(newLine);
-            }
-
-            for (HtmlElement e : elements)
-                sb.append(e.toStringImpl(indent + 1));
-
-            sb.append(String.format("%s</%s>%s", i, name, newLine));
-            return sb.toString();
-        }
-
-        @Override
-        public String toString() {
-            return toStringImpl(0);
-        }
+    public HtmlElement()
+    {
     }
 
-    static class HtmlBuilder {
-
-        private String rootName;
-        private HtmlElement root = new HtmlElement();
-
-        public HtmlBuilder(String rootName) {
-            this.rootName = rootName;
-            root.name = rootName;
-        }
-
-        public void addChild(String childName, String childText) {
-            HtmlElement e = new HtmlElement(childName, childText);
-            root.elements.add(e);
-        }
-
-        public void clear() {
-            root = new HtmlElement();
-            root.name = rootName;
-        }
-
-        @Override
-        public String toString() {
-            return root.toString();
-        }
+    public HtmlElement(String name, String text)
+    {
+        this.name = name;
+        this.text = text;
     }
 
-    public static void main(String[] args) {
+    private String toStringImpl(int indent)
+    {
+        StringBuilder sb = new StringBuilder();
+        String i = String.join("", Collections.nCopies(indent * indentSize, " "));
+        sb.append(String.format("%s<%s>%s", i, name, newLine));
+        if (text != null && !text.isEmpty())
+        {
+            sb.append(String.join("", Collections.nCopies(indentSize*(indent+1), " ")))
+                    .append(text)
+                    .append(newLine);
+        }
 
+        for (HtmlElement e : elements)
+            sb.append(e.toStringImpl(indent + 1));
+
+        sb.append(String.format("%s</%s>%s", i, name, newLine));
+        return sb.toString();
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringImpl(0);
+    }
+}
+
+class HtmlBuilder
+{
+    private String rootName;
+    private HtmlElement root = new HtmlElement();
+
+    public HtmlBuilder(String rootName)
+    {
+        this.rootName = rootName;
+        root.name = rootName;
+    }
+
+    // not fluent
+    public void addChild(String childName, String childText)
+    {
+        HtmlElement e = new HtmlElement(childName, childText);
+        root.elements.add(e);
+    }
+
+    public HtmlBuilder addChildFluent(String childName, String childText)
+    {
+        HtmlElement e = new HtmlElement(childName, childText);
+        root.elements.add(e);
+        return this;
+    }
+
+    public void clear()
+    {
+        root = new HtmlElement();
+        root.name = rootName;
+    }
+
+    // delegating
+    @Override
+    public String toString()
+    {
+        return root.toString();
+    }
+}
+
+class BuildersPattern
+{
+    public static void main(String[] args)
+    {
         // we want to build a simple HTML paragraph
         System.out.println("Testing");
         String hello = "hello";
@@ -103,6 +114,12 @@ public class BuildersPattern {
         HtmlBuilder builder = new HtmlBuilder("ul");
         builder.addChild("li", "hello");
         builder.addChild("li", "world");
+        System.out.println(builder);
+
+        // fluent builder
+        builder.clear();
+        builder.addChildFluent("li", "hello")
+                .addChildFluent("li", "world");
         System.out.println(builder);
     }
 }
