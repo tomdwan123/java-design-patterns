c39f321736dd5251f84faa655277b5097bcf29ca
phuc.letan <phuc.letan@messagemedia.com>
2023-04-25 14:54:53 +0700

    Builders Pattern example

--- src/com/phucdevs/creation/builder/BuildersPattern.java
+++ src/com/phucdevs/creation/builder/BuildersPattern.java
@@ -1,18 +1,108 @@
 package com.phucdevs.creation.builder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BuildersPattern {
 
-    public static void main(String[] args) {
-        String hello = "hello";
-        System.out.println("<p" + hello + "</p>");
+    static class HtmlElement {
 
-        String[] words = {"hello", "world"};
+        public String name, text;
+        public ArrayList<HtmlElement> elements = new ArrayList<>();
+        private final int indentSize = 2;
+        private final String newLine = System.lineSeparator();
+
+        public HtmlElement() {}
+
+        public HtmlElement(String name, String text) {
+            this.name = name;
+            this.text = text;
+        }
+
+        public HtmlElement(String name, String text, ArrayList<HtmlElement> elements) {
+            this.name = name;
+            this.text = text;
+            this.elements = elements;
+        }
+
+        private String toStringImpl(int indent) {
+
+            StringBuilder sb = new StringBuilder();
+            String i = String.join("", Collections.nCopies(indent * indentSize, " "));
+            sb.append(String.format("%s<%s>%s", i, name, newLine));
+            if (text != null && !text.isEmpty())
+            {
+                sb.append(String.join("", Collections.nCopies(indentSize*(indent+1), " ")))
+                        .append(text)
+                        .append(newLine);
+            }
+
+            for (HtmlElement e : elements)
+                sb.append(e.toStringImpl(indent + 1));
+
+            sb.append(String.format("%s</%s>%s", i, name, newLine));
+            return sb.toString();
+        }
+
+        @Override
+        public String toString() {
+            return toStringImpl(0);
+        }
+    }
+
+    static class HtmlBuilder {
+
+        private String rootName;
+        private HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(String rootName) {
+            this.rootName = rootName;
+            root.name = rootName;
+        }
+
+        public void addChild(String childName, String childText) {
+            HtmlElement e = new HtmlElement(childName, childText);
+            root.elements.add(e);
+        }
+
+        public void clear() {
+            root = new HtmlElement();
+            root.name = rootName;
+        }
+
+        @Override
+        public String toString() {
+            return root.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // we want to build a simple HTML paragraph
+        System.out.println("Testing");
+        String hello = "hello";
         StringBuilder sb = new StringBuilder();
+        sb.append("<p>")
+                .append(hello)
+                .append("</p>"); // a builder!
+        System.out.println(sb);
+
+        // now we want to build a list with 2 words
+        String [] words = {"hello", "world"};
+        sb.setLength(0); // clear it
         sb.append("<ul>\n");
-        for (String word : words) {
-            sb.append(String.format(" <li>%s<li>\n", word));
+        for (String word: words)
+        {
+            // indentation management, line breaks and other evils
+            sb.append(String.format("  <li>%s</li>\n", word));
         }
         sb.append("</ul>");
         System.out.println(sb);
+
+        // ordinary non-fluent builder
+        HtmlBuilder builder = new HtmlBuilder("ul");
+        builder.addChild("li", "hello");
+        builder.addChild("li", "world");
+        System.out.println(builder);
     }
 }
