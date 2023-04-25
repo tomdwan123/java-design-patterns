39fb9689e4645081c4a7bad4a71f7ba3dc995e1f
phuc.letan <phuc.letan@messagemedia.com>
2023-04-25 21:14:35 +0700

    Singleton enum example

---
+++ src/com/phucdevs/creation/singleton/SingletonEnumBased.java
@@ -0,0 +1,56 @@
+package com.phucdevs.creation.singleton;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SingletonEnumBased {
+
+    public static void main(String[] args) throws Exception {
+
+        String filename = "myfile.bin";
+        EnumBasedSingleton singleton = EnumBasedSingleton.INSTANCE;
+        singleton.setValue(111);
+        writeToFile(singleton, filename);
+
+        EnumBasedSingleton singleton2 = readFromFile(filename);
+        System.out.println(singleton2.getValue());
+    }
+
+    static void writeToFile(EnumBasedSingleton singleton,
+                            String filename) throws Exception {
+
+        try (FileOutputStream fileOut = new FileOutputStream(filename);
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(singleton);
+        }
+    }
+
+    static EnumBasedSingleton readFromFile(String filename) throws Exception {
+
+        try (FileInputStream fileIn = new FileInputStream(filename);
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            return (EnumBasedSingleton) in.readObject();
+        }
+    }
+}
+
+enum EnumBasedSingleton {
+
+    INSTANCE;
+
+    EnumBasedSingleton() {
+        value = 42;
+    }
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
