92d0d1d2af878dcfc4b336af018504ff020cd9df
Phuc Le <tomdwanno1@gmail.com>
2023-04-27 11:46:15 +0700

    Proxy protection example

---
+++ src/com/phucdevs/structural/proxy/ProxyProtection.java
@@ -0,0 +1,54 @@
+package com.phucdevs.structural.proxy;
+
+public class ProxyProtection {
+
+    public static void main(String[] args) {
+        Car car = new Car(new Driver(12));
+        car.drive();
+        CarProxy carProxy = new CarProxy(new Driver(12));
+        carProxy.drive();
+    }
+}
+
+interface Drivable {
+    void drive();
+}
+
+class Car implements Drivable {
+
+    protected Driver driver;
+
+    public Car(Driver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Car being driven");
+    }
+}
+
+class Driver {
+
+    public int age;
+
+    public Driver(int age) {
+        this.age = age;
+    }
+}
+
+class CarProxy extends Car {
+
+    public CarProxy(Driver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void drive() {
+        if (driver.age >= 16) {
+            super.drive();
+        } else {
+            System.out.println("Driver too young!");
+        }
+    }
+}
